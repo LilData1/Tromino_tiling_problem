90fa6811f1af7dcba99130dab42064b03990e708
lildata1 <Cam.jherb@gmail.com>
2022-03-21 16:38:30 +1300

    changed boolean to ints for board still need to fix bugs and implement color

--- src/A1.java
+++ src/A1.java
@@ -1,8 +1,8 @@
 
 class A1 {
 
-    static boolean[][] board = new boolean[0][0];
-
+    static int[][] board = new int[0][0];
+    static int count = 1;
 
     public static void main(String[] args){
 
@@ -11,39 +11,28 @@ class A1 {
         location L = new location(7,0);
 
         location b = new location(0,0);
-        board = new boolean[n][n];
-        board[L.getX()][L.getY()]=true;
+        board = new int[n][n];
+        board[L.getX()][L.getY()]=count;
 
         //board[0][1]=true;
 
         tile(n, L, b);
 
-
-
-        for(int i = 0; i<n; i++){
-            for(int y = n-1; y>=0; y--){
-                if(board[i][y])System.out.print("X|");
-                else System.out.print(" |");
-
-            }
-
-            System.out.println(" ");
-            System.out.println("----------------------------------------------");
         }
-    }
+
+
 
 
     static void tile(int n, location L, location b) {
-        if (n == 2) return;
+        count++;
+
+
 
 
         for(int i = 8-1; i>=0; i--){
             System.out.print("|");
             for(int y = 0; y<8; y++){
-            //for(int y = 8-1; y>=0; y--){
-                if(board[y][i])System.out.print("X|");
-                else System.out.print(" |");
-
+                System.out.print(board[y][i]+"|");
             }
 
             System.out.println(" ");
@@ -51,12 +40,29 @@ class A1 {
         }
         System.out.println(" ");
 
-        System.out.println(" ");
+
+        if (n == 2) {
+            int int1 = board[b.getX()][b.getY()];
+            int int2 = board[b.getX()+1][b.getY()];
+            int int3 = board[b.getX()][b.getY()+1];
+            int int4= board[b.getX()+1][b.getY()+1];
+
+            if(int1!=0 || int2!=0 || int3!=0 || int4!=0) {
+                if (int1 == 0) board[b.getX()][b.getY()] = count;
+                if (int2 == 0) board[b.getX() + 1][b.getY()] = count;
+                if (int3 == 0) board[b.getX()][b.getY() + 1] = count;
+                if (int4 == 0) board[b.getX() + 1][b.getY() + 1] = count;
+            }
+
+            return;
+        }
+
+        /*System.out.println(" ");
         System.out.println(n);
         System.out.println("Missing: "+ L.getX() +" "+ L.getY());
         System.out.println("bot: "+ b.getX() +" "+ b.getY());
         System.out.println("Middle: "+n/2);
-        System.out.println(" ");
+        System.out.println(" ");*/
 
 
 
@@ -65,13 +71,13 @@ class A1 {
 
 
         //Right
-        if (L.getX() + b.getX() > (n / 2)) {
+        if (L.getX() - b.getX() > (n / 2)) {
             //Top
-            if (L.getY() + b.getY() > (n / 2)) {
+            if (L.getY() - b.getY() > (n / 2)) {
                 System.out.println("TR");
-                board[(n / 2 - 1)+b.getX()][(n / 2 - 1)+b.getY()] = true;
-                board[(n / 2)+b.getX()][(n / 2 - 1)+b.getY()] = true;
-                board[(n / 2 - 1)+b.getX()][(n / 2)+b.getY()] = true;
+                board[(n / 2 - 1)+b.getX()][(n / 2 - 1)+b.getY()] = count;
+                board[(n / 2)+b.getX()][(n / 2 - 1)+b.getY()] = count;
+                board[(n / 2 - 1)+b.getX()][(n / 2)+b.getY()] = count;
 
 
                 tile(n/2, L, new location(n/2,n/2));
@@ -87,9 +93,9 @@ class A1 {
             //Bot
             else {
                 System.out.println("BR");
-                board[(n / 2)+b.getX()][(n / 2)+b.getY()] = true;
-                board[(n / 2 - 1)+b.getX()][(n / 2)+b.getY()] = true;
-                board[(n / 2 - 1)+b.getX()][(n / 2 - 1)+b.getY()] = true;
+                board[(n / 2)+b.getX()][(n / 2)+b.getY()] = count;
+                board[(n / 2 - 1)+b.getX()][(n / 2)+b.getY()] = count;
+                board[(n / 2 - 1)+b.getX()][(n / 2 - 1)+b.getY()] = count;
 
                 //BR
                 tile(n/2, L, new location(n/2,0));
@@ -104,11 +110,11 @@ class A1 {
         }   //Left
             else{
                 //Top
-                if (L.getY() + b.getY() > (n / 2)) {
+                if (L.getY() - b.getY() > (n / 2)) {
                     System.out.println("TL");
-                    board[(n / 2)+b.getX()][(n / 2)+b.getY()] = true;
-                    board[(n / 2 - 1)+b.getX()][(n / 2 - 1)+b.getY()] = true;
-                    board[(n / 2)+b.getX()][(n / 2 - 1)+b.getY()] = true;
+                    board[(n / 2)+b.getX()][(n / 2)+b.getY()] = count;
+                    board[(n / 2 - 1)+b.getX()][(n / 2 - 1)+b.getY()] = count;
+                    board[(n / 2)+b.getX()][(n / 2 - 1)+b.getY()] = count;
 
                     //TL
                     tile(n/2, L, new location(0,n/2));
@@ -123,9 +129,9 @@ class A1 {
                 //Bot
                 else {
                     System.out.println("BL");
-                    board[(n / 2 - 1)+b.getX()][(n / 2)+b.getY()] = true;
-                    board[(n / 2)+b.getX()][(n / 2 - 1)+b.getY()] = true;
-                    board[(n / 2)+b.getX()][(n / 2)+b.getY()] = true;
+                    board[(n / 2 - 1)+b.getX()][(n / 2)+b.getY()] = count;
+                    board[(n / 2)+b.getX()][(n / 2 - 1)+b.getY()] = count;
+                    board[(n / 2)+b.getX()][(n / 2)+b.getY()] = count;
 
                     //BL
                     tile(n/2, L, b);
