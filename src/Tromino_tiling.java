614fec31e037906dd5aca20189eae7f4034d659f
camjh <Cam.jherb@gmail.com>
2022-03-23 14:11:33 +1300

    submission, works for 4x4 but still has bugs for larger

--- src/Tromino_tiling.java
+++ src/Tromino_tiling.java
@@ -1,6 +1,10 @@
 import java.awt.*;
 import java.util.ArrayList;
 
+/**
+ * Sorry couldnt get this to fully work.
+ * It still has bugs for 8x8 and 16x16 but should still work for 4x4
+ */
 class Tromino_tiling {
 
     static int[][] board = new int[0][0];
@@ -10,12 +14,13 @@ class Tromino_tiling {
 
         location b = new location(0, 0);
 
-        int n = 8;
+        //n = size of board
+        int n = 16;
+        //L = location of missing square x=0, y=0 - x=n-1, y=n-1
         location L = new location(0, 0);
+
         tile(n, L, b);
 
-        System.out.println(trominos.size());
-        System.out.println("");
         for (tromino x : trominos){
             System.out.println(x);
         }
@@ -37,14 +42,7 @@ class Tromino_tiling {
 
     static void tile(int n, location L, location b) {
         String temp = "";
-        System.out.println(n);
-        System.out.println(L);
-        System.out.println(b);
         if (n==2){
-
-            //System.out.println(n);
-            //System.out.println(L);
-            //System.out.println(b);
             if(L.getX()==0 && L.getY()==0) temp = "UR";
             if(L.getX()==0 && L.getY()==1) temp = "LR";
             if(L.getX()==1 && L.getY()==0) temp = "UL";
@@ -74,15 +72,12 @@ class Tromino_tiling {
 
          trominos.add(new tromino( new location(b.getX()+(n/2), b.getY()+(n/2)), temp));
 
-        System.out.println(temp);
-        System.out.println(n);
-        System.out.println("");
 
         //BL
         if(temp!="UR") tile(n/2, new location(n/2-1, n/2-1), b);
         else tile(n/2, L, b);
         //BR
-        if(temp!="UL") tile(n/2, new location(n/2-2, n/2-1), new location(b.getX()+n/2, b.getY()));
+        if(temp!="UL") tile(n/2, new location(n/2, n/2-1), new location(b.getX()+n/2, b.getY()));
         else tile(n/2, new location(L.getX()-n/2, L.getY()), new location(b.getX()+n/2, b.getY()));
         //TL
         if(temp!="LR") tile(n/2, new location(n/2-1, n/2-2), new location(b.getX(), b.getY()+n/2));
@@ -91,20 +86,7 @@ class Tromino_tiling {
         if(temp!="LL") tile(n/2, new location(n/2-2, n/2-2), new location(b.getX()+n/2, b.getY()+n/2));
         else tile(n/2, new location(L.getX()-n/2, L.getY()-n/2), new location(b.getX()+n/2, b.getY()+n/2));
 
-    /*
-    //BL
-        if(temp!="UR") tile(n/2, new location(n/2-1, n/2-1), b);
-        else tile(n/2, L, b);
-        //BR
-        if(temp!="UL") tile(n/2, new location(n/2-2, n/2-1), new location(b.getX()+n/2, b.getY()));
-        else tile(n/2, new location(L.getX()-n/2, L.getY()), new location(b.getX()+n/2, b.getY()));
-        //TL
-        if(temp!="LR") tile(n/2, new location(n/2-1, n/2-2), new location(b.getX(), b.getY()+n/2));
-        else tile(n/2, new location(L.getX(), L.getY()-n/2), new location(b.getX(), b.getY()+n/2));
-        //TR
-        if(temp!="LL") tile(n/2, new location(n/2-2, n/2-2), new location(b.getX()+n/2, b.getY()+n/2));
-        else tile(n/2, new location(L.getX()-n/2, L.getY()-n/2), new location(b.getX()+n/2, b.getY()+n/2));
-     */
+
     }
 
 }
